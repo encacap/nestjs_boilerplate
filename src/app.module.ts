3af30d5a426c977150dc56300affc645c8f5d68a
Khanh Nguyen <khanh.nguyen@techupcorp.com>
2022-07-20 21:34:53 +0700

    feat(database): setup MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-    imports: [],
-    controllers: [AppController],
-    providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: ['.env.development', '.env.production'],
+            validationSchema: Joi.object({
+                NODE_ENV: Joi.string().valid('development', 'production', 'test').default('development'),
+                PORT: Joi.number().default(3000),
+                MONGO_URL: Joi.string().required(),
+            }),
+            validationOptions: { stripUnknown: true, abortEarly: true },
+            expandVariables: true,
+            isGlobal: true,
+        }),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                uri: configService.get('MONGO_URL'),
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            }),
+            inject: [ConfigService],
+        }),
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
