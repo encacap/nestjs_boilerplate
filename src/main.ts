e1a1738c5204c6b208773c179431c8a48c6da265
Khanh Nguyen <khanh.nguyen@techupcorp.com>
2022-07-21 12:46:39 +0700

    feat(user): complete validation middleware

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -7,6 +8,13 @@ async function bootstrap() {
     const configService = app.get(ConfigService);
 
     const PORT = configService.get('PORT');
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: false,
+            stopAtFirstError: false,
+        }),
+    );
 
     await app.listen(PORT);
 }
--- src/modules/users/dto/createUser.dto.ts
+++ src/modules/users/dto/createUser.dto.ts
@@ -1,6 +1,17 @@
+import { IsEmail, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+    @IsEmail()
     email: string;
+
+    @IsNotEmpty()
+    @MinLength(6)
+    @MaxLength(20)
     password: string;
+
+    @IsNotEmpty()
     firstName: string;
+
+    @IsNotEmpty()
     lastName: string;
 }
